57348343cafd308e5b4fce6f1322a07d9c315dac
Hajara <hajaraiyal@gmail.com>
2022-02-22 09:59:12 +0000

    add user to event and remove user from event tests added

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { addUser, getEventsByUserId, removeUserFromEvent, getAllUsers } from "../models/users.js";
+import {
+	addUserToEvent,
+	getEventsByUserId,
+	removeUserFromEvent,
+	getAllUsers,
+} from "../models/users.js";
 
 const usersRouter = express.Router();
 // get events according to user id
@@ -17,40 +22,40 @@ const usersRouter = express.Router();
 // });
 
 usersRouter.get("/", async (req, res) => {
-  const data = await getAllUsers();
-  res.json({
-    sucess: true,
-    payload: data,
-  });
-})
+	const data = await getAllUsers();
+	res.json({
+		success: true,
+		payload: data,
+	});
+});
 
 // add user against event chosen to attend
 usersRouter.post("/", async (req, res) => {
-  const { auth_id, event_attend } = req.body;
-  try {
-    const data = await addUser(auth_id, event_attend);
-    res.json({
-      sucess: true,
-      payload: data,
-    });
-  } catch (error) {
-    return res.status(400).json({ error: error.toString() });
-  }
+	const { auth_id, event_attend } = req.body;
+	try {
+		const data = await addUserToEvent(auth_id, event_attend);
+		res.json({
+			success: true,
+			payload: data,
+		});
+	} catch (error) {
+		return res.status(400).json({ error: error.toString() });
+	}
 });
 
 // add a delete for user if they chose to not attend
 export default usersRouter;
 
-// remove a user from an event with event_attend and user id 
+// remove a user from an event with event_attend and user id
 usersRouter.delete("/", async (req, res) => {
-  const { auth_id, event_attend } = req.body;
-  try {
-    const data = await removeUserFromEvent(auth_id, event_attend);
-    res.json({
-      sucess: true,
-      message: "User has been removed from the event"
-    });
-  } catch (error) {
-    return res.status(400).json({ error: error.toString() });
-  }
-})
+	const { auth_id, event_attend } = req.body;
+	try {
+		const data = await removeUserFromEvent(auth_id, event_attend);
+		res.json({
+			success: true,
+			message: "User has been removed from the event",
+		});
+	} catch (error) {
+		return res.status(400).json({ error: error.toString() });
+	}
+});
--- routes/users.test.js
+++ routes/users.test.js
@@ -1,46 +1,70 @@
 import request from "supertest";
 import usersRouter from "./users.js";
-import { getAllUsers, addUser, removeUserFromEvent } from "../models/users";
+import {
+	getAllUsers,
+	addUserToEvent,
+	removeUserFromEvent,
+} from "../models/users";
 
 describe("testing our HTTP requests at /users", function () {
-    // Testing that the response object contains an array of objects (events) 
-    it("should return an array of objects containing all users when listening to a GET request", async () => {
-        // ACT
-        const actual = await getAllUsers();
-        request(usersRouter)
-            .get('/')
-            .expect(200)
-            .expect(res => {
-                // ARRANGE
-                const expected = {
-                    success: true,
-                    payload: expect.arrayContaining([{
-                        user_id: expect.any(Number),
-                        auth_id: expect.any(String),
-                        event_attend: expect.any(Number)
-                    }])
-                }
-                // ASSERT
-                expect(actual).toStrictEqual(expected);
-            })
-    })
-    it("should return a object containing the user and the event they are attending when listening to a POST request", async () => {
-        // ACT
-        const actual = await addUser();
-        request(usersRouter)
-            .get('/')
-            .expect(200)
-            .expect(res => {
-                // ARRANGE
-                const expected = {
-                    success: true,
-                    payload: expect.arrayContaining([{
-                        auth_id: expect.any(String),
-                        event_attend: expect.any(Number)
-                    }])
-                }
-                // ASSERT
-                expect(actual).toStrictEqual(expected);
-            })
-    })
-})
+	// Testing that the response object contains an array of objects (events)
+	it("should return an array of objects containing all users when listening to a GET request", async () => {
+		// ACT
+		const actual = await getAllUsers();
+		request(usersRouter)
+			.get("/")
+			.expect(200)
+			.expect((res) => {
+				// ARRANGE
+				const expected = {
+					success: true,
+					payload: expect.arrayContaining([
+						{
+							user_id: expect.any(Number),
+							auth_id: expect.any(String),
+							event_attend: expect.any(Number),
+						},
+					]),
+				};
+				// ASSERT
+				expect(actual).toStrictEqual(expected);
+			});
+	});
+	it("should return a object containing the user and the event they are attending when listening to a POST request", async () => {
+		// ACT
+		const actual = await addUserToEvent();
+		request(usersRouter)
+			.get("/")
+			.expect(200)
+			.expect((res) => {
+				// ARRANGE
+				const expected = {
+					success: true,
+					payload: expect.arrayContaining([
+						{
+							auth_id: expect.any(String),
+							event_attend: expect.any(Number),
+						},
+					]),
+				};
+				// ASSERT
+				expect(actual).toStrictEqual(expected);
+			});
+	});
+	it("should return a object containing the user and the event they are attending when listening to a POST request", async () => {
+		// ACT
+		const actual = await removeUserFromEvent();
+		request(usersRouter)
+			.get("/")
+			.expect(200)
+			.expect((res) => {
+				// ARRANGE
+				const expected = {
+					success: true,
+					message: expect.toEqual("User has been removed from the event"),
+				};
+				// ASSERT
+				expect(actual).toStrictEqual(expected);
+			});
+	});
+});
